1452b6798f08a1bcf9fef3fc927cf2ed701ad05f
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2018-08-10 13:15:44 +0100

    Set default args for rubyLinter
    These were removed in https://github.com/alphagov/govuk-jenkinslib/pull/21 and is causing jobs that use this method to fail e.g: https://github.com/alphagov/govuk-developer-docs/blob/master/Jenkinsfile#L39

--- vars/govuk.groovy
+++ vars/govuk.groovy
@@ -512,7 +512,7 @@ def setEnvGitCommit() {
 /**
  * Runs the ruby linter. Only lint commits that are not in master.
  */
-def rubyLinter(String dirs = 'app spec lib', boolean lintDiff, boolean lintRails) {
+def rubyLinter(String dirs = 'app spec lib', boolean lintDiff = true, boolean lintRails = false) {
   setEnvGitCommit()
   if (!isCurrentCommitOnMaster()) {
     echo 'Running Ruby linter'
